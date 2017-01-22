0079fcef0f7173a7284deb5ceea43f211883a8a1
Maya Ayoubi <mayaayoubi2012@gmail.com>
2017-01-21 22:42:14 -0500

    Update Pokemanz repo

--- New Pokemon Dataset/pokemon_new.R
+++ New Pokemon Dataset/pokemon_new.R
@@ -3,7 +3,7 @@ library(ggplot2)
 
 pokemon <- read.csv("C://Users//lenovo//Desktop//pokemon_new.csv")
 
-pokemon <- subset(pokemon, Index>721 & Index<803)
+pokemanz <- subset(pokemon, Index>721 & Index<803)
 
-stats <- pokemon[ ,c(7,8,9,10,11,12)]
+stats <- pokemanz[ ,c(7,8,9,10,11,12)]
 summary(stats)
